bf5460136b81d9da4bbace07888feae559fb8273
akimanizanye.claudine@gmail.com <akimanizanye.claudine@gmail.com>
2019-03-25 10:07:55 +0200

    i added the url of user account

--- Instagram/models.py
+++ Instagram/models.py
@@ -12,7 +12,7 @@ class Image(models.Model):
     image = models.ImageField(upload_to = 'insta/')
     name = models.CharField(max_length =30)
     image_Caption = models.CharField(max_length=50)
-    Profile = models..ForeignKey(Profile)
+    Profile = models.ForeignKey(Profile)
     likes = models.CharField(max_length=40)
     Comments = models.CharField(max_length=60)
 
@@ -29,19 +29,19 @@ class Image(models.Model):
         self.objects.filter(id = self.pk).update(**kwargs)
 
 class  Follow(models.Model):
-    profile_id = models.IntField()
-    user_id =models.IntField()
+    profile_id = models.IntegerField()
+    user_id =models.IntegerField()
     def __str__(self):
         return self.name
 
    
 
-class Comment(db.Model):
+# class Comment(models.Model):
 
-    id = models.IntField(primary_key = True)
-    comment= models.CharField(max_length =30)
-    image_id =models.ForeignKey('Image.id'))
-    user_id = models.ForeignKey("users.id"))
+#     id = models.IntegerField(primary_key = True)
+#     comment= models.CharField(max_length =30)
+#     image_id =models.ForeignKey('Image.id')
+#     user_id = models.ForeignKey("users.id")
     
 
 
--- images/urls.py
+++ images/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^Instagram/',include('Instagram.urls'))
+    url(r'^Instagram/',include('Instagram.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls'))
 ]
