e1198f892c91d85fc55fb99939745ce6b8e7d47d
akimanizanye.claudine@gmail.com <akimanizanye.claudine@gmail.com>
2019-03-25 09:56:56 +0200

    I created the instagram app in the setting.py

--- Instagram/models.py
+++ Instagram/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
      bio=models.CharField(max_length=50)  
 
 class Image(models.Model):
-   image = models.ImageField(upload_to = 'insta/')
+    image = models.ImageField(upload_to = 'insta/')
     name = models.CharField(max_length =30)
     image_Caption = models.CharField(max_length=50)
     Profile = models..ForeignKey(Profile)
@@ -27,6 +27,21 @@ class Image(models.Model):
         self.delete()
     def update_caption(self, **kwargs):
         self.objects.filter(id = self.pk).update(**kwargs)
+
+class  Follow(models.Model):
+    profile_id = models.IntField()
+    user_id =models.IntField()
+    def __str__(self):
+        return self.name
+
+   
+
+class Comment(db.Model):
+
+    id = models.IntField(primary_key = True)
+    comment= models.CharField(max_length =30)
+    image_id =models.ForeignKey('Image.id'))
+    user_id = models.ForeignKey("users.id"))
     
 
 
