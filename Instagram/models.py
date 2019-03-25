28d595fda9bd4a93c99b3e105edcbc0d0373bd3a
akimanizanye.claudine@gmail.com <akimanizanye.claudine@gmail.com>
2019-03-25 08:46:57 +0200

    I created the ImaGE AND profile models

--- Instagram/models.py
+++ Instagram/models.py
@@ -3,4 +3,23 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+   image = models.CharField(max_length =30)
+    image_namr = models.CharField(max_length =30)
+    image_Caption= models.CharField(max_length=50)
+    Profile=models.CharField(max_length=50)
+    likes=models.CharField(max_length=40)
+    Comments= models.CharField(max_length=60)
+
+
+
+
+
+
+
+
+
+
+ class Profile(models.Model):
+     profile_photo=models.CharField(max_length=40)
+     bio=models.CharField(max_length=50)    
