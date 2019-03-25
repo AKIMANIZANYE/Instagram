362dbff29315e477f3fa2644fcaeced51adbbae9
akimanizanye.claudine@gmail.com <akimanizanye.claudine@gmail.com>
2019-03-25 13:12:32 +0200

    i created an account

--- images/urls.py
+++ images/urls.py
@@ -20,5 +20,5 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^Instagram/',include('Instagram.urls')),
-    
+    url(r'^accounts/', include('registration.backends.simple.urls'))
 ]
