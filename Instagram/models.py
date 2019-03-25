987221c3a780805f9bbb288129c7aad8d26b3351
akimanizanye.claudine@gmail.com <akimanizanye.claudine@gmail.com>
2019-03-25 09:00:25 +0200

    i created the  method  of the  images models

--- Instagram/models.py
+++ Instagram/models.py
@@ -3,23 +3,38 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Image(models.Model):
-   image = models.CharField(max_length =30)
-    image_namr = models.CharField(max_length =30)
-    image_Caption= models.CharField(max_length=50)
-    Profile=models.CharField(max_length=50)
-    likes=models.CharField(max_length=40)
-    Comments= models.CharField(max_length=60)
 
-
-
-
-
-
-
-
-
-
- class Profile(models.Model):
+class Profile(models.Model):
      profile_photo=models.CharField(max_length=40)
-     bio=models.CharField(max_length=50)    
+     bio=models.CharField(max_length=50)  
+
+class Image(models.Model):
+   image = models.ImageField(upload_to = 'insta/')
+    name = models.CharField(max_length =30)
+    image_Caption = models.CharField(max_length=50)
+    Profile = models..ForeignKey(Profile)
+    likes = models.CharField(max_length=40)
+    Comments = models.CharField(max_length=60)
+
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def del_image(self):
+        self.delete()
+    def update_caption(self, **kwargs):
+        self.objects.filter(id = self.pk).update(**kwargs)
+    
+
+
+
+
+
+
+
+
+
+  
