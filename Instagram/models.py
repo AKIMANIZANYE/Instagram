88ad27e91e755d263bed412eee6f4148853527eb
akimanizanye.claudine@gmail.com <akimanizanye.claudine@gmail.com>
2019-03-26 16:58:42 +0200

    I created the comments.html

--- Instagram/models.py
+++ Instagram/models.py
@@ -56,3 +56,56 @@ class Image(models.Model):
 class PhotosLetterRecipients(models.Model):
     name = models.CharField(max_length = 30)
     email = models.EmailField() 
+
+
+
+class Comment(models.Model):
+        user = models.ForeignKey(User,on_delete = models.CASCADE, null= True)
+        image = models.ForeignKey(Image,on_delete = models.CASCADE, null= True,related_name='comment')
+        comment= models.TextField( blank=True)
+        
+        def __str__(self):
+            return self.comment
+
+
+        def delete_comment(self):
+            self.delete()
+
+        def save_comment(self):
+            self.save()
+
+
+class Follow(models.Model):
+	user = models.ForeignKey(Profile,on_delete = models.CASCADE,null=True)
+	follower = models.ForeignKey(User,on_delete = models.CASCADE,null=True)
+
+	def __int__(self):
+		return self.name
+
+	def save_follower(self):
+		self.save()
+
+	def delete_follower(self):
+		self.save()
+
+
+class Unfollow(models.Model):
+	user = models.ForeignKey(Profile,on_delete = models.CASCADE,null=True)
+	follower = models.ForeignKey(User,on_delete = models.CASCADE,null=True)
+
+	def __int__(self):
+		return self.name
+
+
+class Likes(models.Model):
+	user = models.ForeignKey(Profile,on_delete = models.CASCADE,null=True)
+	
+
+	def __int__(self):
+		return self.name
+
+	def unlike(self):
+		self.delete()
+
+	def save_like(self):
+		self.save()     
