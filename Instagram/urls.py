14de04f2e43f00368126db5ef0254b8fd54f42a5
akimanizanye.claudine@gmail.com <akimanizanye.claudine@gmail.com>
2019-03-26 12:49:30 +0200

    I created the  registration form

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url(r'^Instagram/image$', views.new_image, name='new-image'),
+     url(r'^image/(\d+)',views.image,name ='image'),
+    url(r'^$', views.photos_today, name = 'photosToday'),
+    # url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastNews'),
     url(r'^search/', views.search_results, name='search_results'),
-    
+    url(r'^new/image$', views.new_image, name='new-image')
 ]
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
